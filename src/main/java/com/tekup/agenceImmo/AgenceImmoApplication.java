29a34f8670b5003fcd5ddc7798d9151996ae0b12
dabbabi <dabbabi.hsan@gmail.com>
2023-01-25 16:27:55 +0100

    initial commit

---
+++ src/main/java/com/tekup/agenceImmo/AgenceImmoApplication.java
@@ -0,0 +1,13 @@
+package com.tekup.agenceImmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenceImmoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenceImmoApplication.class, args);
+	}
+
+}
